26d5038b25f130c65af699e341f1cdbefd820f4a
tantohtelmah <tantohtelmah@gmail.com>
2026-06-30 19:13:50 -0400

    Completed an experience at TradeBinder

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -27,7 +27,7 @@ export const experience = [
   {
     role: "Frontend Developer",
     company: "TradeBinder",
-    date: "Dec 2025 – Present",
+    date: "Dec 2025 – May 2025",
     points: ["Angular", "Ionic", "TypeScript", "API integrations"]
   },
   {
